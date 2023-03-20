ffee3f564e04ff77b64161994df934b1902d56cf
Jesse <jessewambu@zetech.ac.ke>
2023-03-20 08:12:42 +0300

    Done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,13 +3,13 @@ class DirectorsController < ApplicationController
 
   def index
     directors = Director.all
-    render json: directors
+    render json: directors, include: ['movies', 'movies.reviews']
   end
 
   def show
     director = Director.find(params[:id])
-    render json: director
-  end
+    render json: directors, include: ['movies', 'movies.reviews']
+    end
 
   private
 
